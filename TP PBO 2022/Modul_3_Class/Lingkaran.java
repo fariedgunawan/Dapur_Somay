9f246eeb79d8623acc782a3a01087d3c85e6c981
Yohanes Januarico Andiawan <yohan.loadout@gmail.com>
2024-09-22 15:48:40 +0700

    penjelasan
    Ngasih penjelasan buat double sama float

--- TP PBO 2022/Modul_3_Class/Lingkaran.java
+++ TP PBO 2022/Modul_3_Class/Lingkaran.java
@@ -1,20 +1,30 @@
 public class Lingkaran {
     String warna;
-    float jariJari; // tipe data float harus ditambahin 'f' diakhir angkanya
+    float jariJariFLOAT; // tipe data float harus ditambahin 'f' diakhir angkanya
+    double jariJariDOUBLE;
 
     void printWarna(){
         System.out.println("Warna objek lingkarang adalah "+warna);
     }
     
+    // ada 2 cara buat perhitungan floating point
+    // kalau mau di masukkin ke variabel gini, enaknya pake float
+    // double klo pembagian jadi aneh
     void printKeliling(){
         float pi = 22/7f;
-        float keliling = 2*pi*jariJari;
+        float keliling = 2*pi*jariJariFLOAT;
         System.out.println("Keliling objek lingkaran adalah "+keliling);
+        // mesti merhatiin urutan perkalian, pi-nya mesti terakhir
+        System.out.println("Keliling objek lingkaran adalah "+jariJariDOUBLE*2*22/7);
     }
     
     void printLuas(){
         float pi = 22/7f;
-        float luas = pi * jariJari * jariJari;
+        float luas = pi * jariJariFLOAT * jariJariFLOAT;
         System.out.println("Luas objek lingkarang adalah "+luas);
+        // mesti merhatiin urutan perkalian, pi-nya mesti terakhir
+        System.out.println("Luas objek lingkarang adalah "+jariJariDOUBLE*jariJariDOUBLE*22/7);
     }
+
+
 }
--- TP PBO 2022/Modul_3_Class/main.java
+++ TP PBO 2022/Modul_3_Class/main.java
@@ -2,7 +2,8 @@ public class main{
     public static void main(String[] args) {
         Lingkaran objek = new Lingkaran(); // instantiate sebuah class 'objek'
         objek.warna = "merah";
-        objek.jariJari = 3.5f;
+        objek.jariJariFLOAT = 3.5f;
+        objek.jariJariDOUBLE = 3.5;
         objek.printWarna();
         objek.printKeliling();
         objek.printLuas();
